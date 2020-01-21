4eae81e1aefe5a6cbae1edfc7906ea520fb36288
ncaldwell17 <noahcaldwell14@yahoo.com>
2020-01-21 14:50:41 -0800

    standard commits

--- visualization.py
+++ visualization.py
@@ -122,3 +122,21 @@ def tfidf(data):
     train = tfidf_vectorizer.fit_transform(data)
 
     return train, tfidf_vectorizer
+
+
+def get_average_word2vec(tokens_list, vector, generate_missing=False, k=300):
+    if len(tokens_list)<1:
+        return np.zeros(k)
+    if generate_missing:
+        vectorized = [vector[word] if word in vector else np.random.rand(k) for word in tokens_list]
+    else:
+        vectorized = [vector[word] if word in vector else np.zeros(k) for word in tokens_list]
+    length = len(vectorized)
+    summed = np.sum(vectorized, axis=0)
+    averaged = np.divide(summed, length)
+    return averaged
+
+def get_word2vec_embeddings(vectors, clean_questions, generate_missing=False):
+    embeddings = clean_questions['tokens'].apply(lambda x: get_average_word2vec(x, vectors, 
+                                                                                generate_missing=generate_missing))
+    return list(embeddings)
