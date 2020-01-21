70011133104ab61d91b67203f7d745871f2342e3
ncaldwell17 <noahcaldwell14@yahoo.com>
2020-01-21 13:20:23 -0800

    standard commits

--- visualization.py
+++ visualization.py
@@ -14,7 +14,54 @@ def plot_LSA(test_data, test_labels, savepath="PCA_demo.csv", plot=True):
         colors = ['orange','blue','blue']
         if plot:
             plt.scatter(lsa_scores[:,0], lsa_scores[:,1], s=8, alpha=.8, c=test_labels, cmap=matplotlib.colors.ListedColormap(colors))
-            red_patch = mpatches.Patch(color='orange', label='Irrelevant')
-            green_patch = mpatches.Patch(color='blue', label='Disaster')
+            red_patch = mpatches.Patch(color='orange', label='Affirm')
+            green_patch = mpatches.Patch(color='blue', label='Reverse')
             plt.legend(handles=[red_patch, green_patch], prop={'size': 30})
 
+
+from sklearn.metrics import accuracy_score, f1_score, precision_score, recall_score, classification_report
+
+def get_metrics(y_test, y_predicted):  
+    # true positives / (true positives+false positives)
+    precision = precision_score(y_test, y_predicted, pos_label=None,
+                                    average='weighted')             
+    # true positives / (true positives + false negatives)
+    recall = recall_score(y_test, y_predicted, pos_label=None,
+                              average='weighted')
+    
+    # harmonic mean of precision and recall
+    f1 = f1_score(y_test, y_predicted, pos_label=None, average='weighted')
+    
+    # true positives + true negatives/ total
+    accuracy = accuracy_score(y_test, y_predicted)
+    return accuracy, precision, recall, f1
+
+import numpy as np
+import itertools
+from sklearn.metrics import confusion_matrix
+
+def plot_confusion_matrix(cm, classes,
+                          normalize=False,
+                          title='Confusion matrix',
+                          cmap=plt.cm.winter):
+    if normalize:
+        cm = cm.astype('float') / cm.sum(axis=1)[:, np.newaxis]
+    plt.imshow(cm, interpolation='nearest', cmap=cmap)
+    plt.title(title, fontsize=30)
+    plt.colorbar()
+    tick_marks = np.arange(len(classes))
+    plt.xticks(tick_marks, classes, fontsize=20)
+    plt.yticks(tick_marks, classes, fontsize=20)
+    
+    fmt = '.2f' if normalize else 'd'
+    thresh = cm.max() / 2.
+
+    for i, j in itertools.product(range(cm.shape[0]), range(cm.shape[1])):
+        plt.text(j, i, format(cm[i, j], fmt), horizontalalignment="center", 
+                 color="white" if cm[i, j] < thresh else "black", fontsize=40)
+    
+    plt.tight_layout()
+    plt.ylabel('True label', fontsize=30)
+    plt.xlabel('Predicted label', fontsize=30)
+
+    return plt
